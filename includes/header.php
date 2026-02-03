9f52d1245ab90db26edadae0947e2be7066c4871
Aravinda Viraj <Aravindaviraj99@gmail.com>
2026-02-03 12:27:02 +0530

    create header

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<header class="slecic-header">
+    <div class="header-branding">
+        <a href="/" class="brand-link">
+            <img src="assets/images/logo/logo_main.png" alt="SLECIC Logo" class="brand-logo">
+        </a>
+    </div>
+
+    <nav class="header-nav">
+        <ul class="nav-list">
+            <li><a href="#" class="nav-link current">Home</a></li>
+            <li><a href="#" class="nav-link">About Us</a></li>
+            <li><a href="#" class="nav-link">Our Services</a></li>
+            <li><a href="#" class="nav-link">News</a></li>
+            <li><a href="#" class="nav-link">Publications</a></li>
+            <li><a href="#" class="nav-link">Downloads</a></li>
+            <li><a href="#" class="nav-link">Contact Us</a></li>
+        </ul>
+    </nav>
+
+    <div class="header-actions">
+        <a href="#" class="btn-client-area">Client Area</a>
+    </div>
+</header>
