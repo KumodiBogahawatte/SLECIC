3dc0337d7e41ca0e2fe6f3d0e02746f3616d2e67
Aravinda Viraj <Aravindaviraj99@gmail.com>
2026-02-03 15:09:14 +0530

    create contact section

--- index.php
+++ index.php
@@ -197,6 +197,53 @@
         </div>
     </section>
 
+    <section class="contact-section">
+    <div class="container">
+        <div class="contact-grid">
+            
+            <div class="contact-form-wrapper">
+                <h2 class="section-title">Get in Touch With Us</h2>
+                <p class="contact-intro">
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+
+                <form action="#" method="POST" class="contact-form">
+                    <div class="form-row">
+                        <input type="text" placeholder="First Name" class="form-control" required>
+                        <input type="text" placeholder="Last Name" class="form-control" required>
+                    </div>
+                    
+                    <div class="form-row">
+                        <input type="email" placeholder="Email" class="form-control" required>
+                        <input type="tel" placeholder="Contact Number" class="form-control">
+                    </div>
+
+                    <textarea placeholder="Description" class="form-control form-textarea" rows="5"></textarea>
+
+                    <button type="submit" class="btn-submit">Submit</button>
+                </form>
+
+                <p class="form-footer-text">
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.
+                </p>
+            </div>
+
+            <div class="contact-map-wrapper">
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.786448375978!2d79.84852757577598!3d6.916124793083466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2594195724103%3A0xe5469490132338b9!2sSri%20Lanka%20Export%20Credit%20Insurance%20Corporation!5e0!3m2!1sen!2slk!4v1700000000000!5m2!1sen!2slk" 
+                    width="100%" 
+                    height="100%" 
+                    style="border:0;" 
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade">
+                </iframe>
+            </div>
+
+        </div>
+    </div>
+    </section>
+
     <?php include 'includes/footer.php'; ?>
 
 </body>
